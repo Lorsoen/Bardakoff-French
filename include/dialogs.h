173d287cf80e1935538799e006ea73b757302085
Lorenzo <fashim99@gmail.com>
2026-02-21 09:38:50 +0100

    Initial

---
+++ include/dialogs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "functions.h"
+
+#define ASSETS_SIZE       912
+#define ASSETS_SIZES_SIZE 200
+
+extern const u8 *asset_data[ASSETS_SIZE];
+extern const u16 asset_name[ASSETS_SIZE];
+extern const u8 asset_size[ASSETS_SIZES_SIZE];
